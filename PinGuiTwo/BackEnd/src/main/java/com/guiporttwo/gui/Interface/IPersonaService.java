cdb43326df1373603c2f5d39d0ef5b8c6f762e4b
Chambalas <105973228+Chambalas@users.noreply.github.com>
2022-11-03 15:39:10 -0300

    Nuevo commit

---
+++ PinGuiTwo/BackEnd/src/main/java/com/guiporttwo/gui/Interface/IPersonaService.java
@@ -0,0 +1,18 @@
+package com.guiporttwo.gui.Interface;
+
+import com.guiporttwo.gui.Entity.Persona;
+import java.util.List;
+
+public interface IPersonaService {
+    //Traer una lista de personas
+    public List<Persona>getPersona();
+    
+    //Guardar un objeto de tipo persona
+    public void savePersona(Persona persona);
+    
+    //Eliminar un objeto buscado por ID
+    public void deletePersona(Long id);
+    
+    //Bucar una persona por ID
+    public Persona findPersona(Long id);
+}
---
+++ PinGuiTwo/BackEnd/src/main/java/com/guiporttwo/gui/Security/Service/RolService.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.guiporttwo.gui.Security.Service;
+
+import com.guiporttwo.gui.Security.Entity.Rol;
+import com.guiporttwo.gui.Security.Enums.RolNombre;
+import com.guiporttwo.gui.Security.Repository.IRolRepository;
+import java.util.Optional;
+import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+public class RolService {
+    @Autowired
+    IRolRepository irolRepository;
+    
+    public Optional<Rol> getByRolNombre(RolNombre rolNombre){
+        return irolRepository.findByRolNombre(rolNombre);
+    }
+    
+    public void save(Rol rol){
+        irolRepository.save(rol);
+    }
+}
